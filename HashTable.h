61e085737ec59f691c641cba750e99fd7622928e
ryanfd <rdonnelly98@gmail.com>
2020-03-07 13:01:19 -0800

    class definition

--- HashTable.h
+++ HashTable.h
@@ -1,2 +1,29 @@
 #pragma once
 
+using std::string;
+
+class HashTable 
+{
+public:
+	// constructors, destructors, operator
+	HashTable();
+	HashTable(int n);
+	HashTable(const HashTable &);
+	~HashTable();
+	HashTable& operator=(const HashTable &);
+
+	// mutators
+	void insert(const string &);
+
+	// accessors
+	bool find(const string &) const;
+	int size() const;
+	int maxSize() const;
+	int loadFactor() const;
+
+private:
+	string* arr;
+	int size;
+	int h2;
+	int currentNumItems;
+};
